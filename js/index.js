fe837d301d1965c617ce88f22b9812c701e7e607
steveneross94 <ross.steven@me.com>
2020-06-29 21:39:30 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const baseUrl = 'http://localhost:3000'
     const booksURL = 'http://localhost:3000/books'
-    const usersUrl = 'http://localhost:3000/users'
+    // const usersUrl = 'http://localhost:3000/users'
     const listPanel = document.querySelector("#list-panel")
     const showPanel = document.querySelector("#show-panel")
-    const bookLi = document.querySelector(".li")
+
 
     const fetchBooks = () => {
         fetch(booksURL)
@@ -19,67 +19,54 @@ document.addEventListener("DOMContentLoaded", function() {
             })
 
             document.addEventListener("click", e => {
-               if (e.target.className === "book"){                   
-                fetch(booksURL+`/${e.target.dataset.id}`)
-                .then(r => r.json())
-                .then(book => {
-                    let users = book.users.map(user => {
-                        return `${user.username}`
-                    })
-                        showPanel.innerHTML = `
-                        <h4>${book.title}</h4>
-                        <img src='${book.img_url}'>
-                        <p>${book.description}</p>
-                        <p>Users: ${users}</p>
-                        <button id=${book.id}>Read Book</button>
-                        `
-                    })               
+               if (e.target.className === "book"){ 
+                   let specificBook = e.target.dataset.id                 
+                    fetchSpecificBook(specificBook)           
                 } else if (e.target.textContent === 'Read Book'){
                     console.log(e.target);
                     let bookId = e.target.id
-                
+               
                     fetch (booksURL+`/${bookId}`)
                     .then(r => r.json())
-                    .then(book => {
-                        book.users.map(user => {
-                            let users = {id: user.id,
-                                 username: user.username}
-                         
-                            
-                         })
-                         
-                    
+                    .then(book => patchBookUsers(book))
+                    // location.reload()
                         
-                        
-                    }
-                    // fetch(booksURL+`/${bookId}`,{
-                    //     method: 'PATCH', 
-                    //     headers: {
-                    //         "content-type": "application/json"
-                    //     },
-                    //     body: JSON.stringify(
-                    //         {
-                    //             "users": [
-                    //             //   {"id":2, "username":"auer"},
-                    //             //   {"id":8, "username":"goodwin"},
-                    //               {"id":1, "username":"pouros"},
-                                
-                    //             ]
-                    //           }
-                    //     )
-                    // })
-                    // .then(r => r.json())
-                    // .then()
-                    
-                   
-                    
-                        )}
-                    
+                }   
             })  
         })
     }
 
- 
+const fetchSpecificBook = specificBook => {
+    fetch(booksURL+`/${specificBook}`)
+    .then(r => r.json())
+    .then(book => getUsers(book)) 
+}
+
+const patchBookUsers = book => {
+        let myUser = {id:1, username: "pouros"}  
+            fetch(booksURL+`/${book.id}`,{
+                method:'PATCH', 
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(
+                    {users: [...book.users, myUser]}
+                )
+                })
+}
+
+const getUsers = (book) => {
+    let users = book.users.map(user => {
+        return `<p class='user'>${user.username}</p>`
+    })
+        showPanel.innerHTML = `
+        <h4>${book.title}</h4>
+        <img src='${book.img_url}'>
+        <p>${book.description}</p>
+        ${users.join("")}
+        <button id='${book.id}'>Read Book</button>
+        `
+    }
  
 fetchBooks();
 });
